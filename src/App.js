8b21c5b194d3c9dcd3eed91dd8619efd5d268cf4
luis-otavio <luisotavio05@gmail.com>
2019-05-19 18:59:58 -0300

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import AppBar from '@material-ui/core/AppBar';
-import './App.css';
+import React from 'react'
+import { createMuiTheme } from '@material-ui/core/styles'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import Header from './pages/common/Header'
+import Home from './pages/home'
+
+
+import './App.css'
 
 const theme = createMuiTheme({
   palette: {
@@ -14,22 +17,15 @@ const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
-});
+})
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <AppBar position="static" color="primary">
-        <div className="header">
-          <p className="title">Lista de produtos</p>
-        </div>
-      </AppBar>
-        <div >
-          <h1>Teste</h1>
-        </div>
-      
+      <Header />
+      <Home />
     </MuiThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
